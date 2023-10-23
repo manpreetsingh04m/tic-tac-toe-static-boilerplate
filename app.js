3ee2b8a5e6b259aec4bdf7a881902a8988416ac6
manpreetsingh04m <ms2404mk@gmail.com>
2023-10-23 15:21:40 +0530

    Static Built

---
+++ app.js
@@ -0,0 +1,13 @@
+var box_container=document.querySelectorAll(".box");
+// List of Combinations 
+let winningCombination=[
+  [1,2,3],//vertical
+  [4,5,6],//vertical
+  [7,8,9],//vertical
+  [1,4,7],//horizontal
+  [2,5,8],//horizontal
+  [3,6,9],//horizontal
+  [1,5,9],
+  [3,5,7],
+]
+
